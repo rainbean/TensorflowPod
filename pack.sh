3b31c5a8a873dc6ab8644c1322564b593e3f672b
Tomer Gafner <tomergafner@gmail.com>
2017-12-13 16:56:32 -0500

    Fix pack script and podspec for tensorflow v1.4

--- pack.sh
+++ pack.sh
@@ -9,4 +9,10 @@ tar rvf $TAR -s '/^./include/' ./tensorflow/contrib/makefile/downloads/eigen/uns
 tar rvf $TAR -s '/^./include/' ./tensorflow/contrib/makefile/downloads/eigen/Eigen
 (cd tensorflow/contrib/makefile/gen; tar rvf ../../../../$TAR lib/libtensorflow-core.a)
 (cd tensorflow/contrib/makefile/gen/protobuf_ios; tar rvf ../../../../../$TAR lib/libprotobuf-lite.a lib/libprotobuf.a)
+# Renaming nsync.a and moving it into a temporary lib directory to have all tensorflow static libraries under the same folder.
+# It would be nice if instead of copy/rename and then remove nsync.a it would be possible to do it all directly with tar.
+mkdir -p tensorflow/contrib/makefile/downloads/nsync/builds/lib
+cp tensorflow/contrib/makefile/downloads/nsync/builds/lipo.ios.c++11/nsync.a tensorflow/contrib/makefile/downloads/nsync/builds/lib/libnsync.a 
+(cd tensorflow/contrib/makefile/downloads/nsync/builds; tar rvf ../../../../../../$TAR lib/libnsync.a)
 gzip $TAR
+rm tensorflow/contrib/makefile/downloads/nsync/builds/lib/libnsync.a
